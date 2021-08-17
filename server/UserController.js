7b86b06b43d902fda22b9dcbfc820453fd9c3a84
Megan <smith.megan.design@gmail.com>
2021-08-17 16:26:01 -0600

    backend work

--- server/UserController.js
+++ server/UserController.js
@@ -4,12 +4,60 @@ let globalUserId = 2;
 let globalPostId = 2;
 
 module.exports = {
-	addUser: (req, res) => {},
-	addPost: (req, res) => {},
-	addComment: (req, res) => {},
-	deleteComment: (req, res) => {},
-	deleteUser: (req, res) => {},
-	updateUser: (req, res) => {},
+	getUser: (req,res)=>{
+		let {username, avatar} = userData
+		let userDisplay={
+			userName
+		}
+		res.status(200).send(userDisplay)
+	},
+	getPost: (req,res)=>{
+		let {post, comment} =userData
+		let postObj={
+			post,
+			comment
+		}
+		res.status(200).send(post)
+	},
+	addUser: (req, res) => {
+		let {username, password, avatar}=req.body
+		let newUser={
+			id: ++globalUserId,
+			username,
+			password,
+		}
+		userData.push(newUser)
+		res.status(200).send("user created")
+	},
+	addPost: (req, res) => {
+		let {post, comment} =userData
+		let postObj={
+			postID: ++globalPostID,
+			post,
+			comment
+		}
+		res.status(200).send(post)
+	},
+	addComment: (req, res) => {
+		let {id, comment_text, comment_Id, post_Id}=req.body
+		let newComment ={
+			comment_text,
+			comment_Id,
+			post_Id
+		}
+		userData.id.comments.push(newComment)
+
+		res.status(200).send("comment created")
+	},
+	deleteComment: (req, res) => {
+	},
+	deleteUser: (req, res) => {
+	},
+	updateUser: (req, res) => {
+		let {id} =req.params;
+		let index=userData.findIndex(e=>e.id===+id)
+		userData.splice(index,1)
+	},
 	updateComment: (req, res) => {},
 	updatePost: (req, res) => {},
 };
