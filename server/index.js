6844c56adc4fe5cbb3efc9c3c10f4430893e7d77
Dylan Schwindt <dfieldmark69@gmail.com>
2021-08-18 07:57:41 -0700

    Login-functionality is like 75% done you get back the user data it gets assigned to a user, with a username checker and password checker

--- server/index.js
+++ server/index.js
@@ -5,38 +5,38 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-let baseURL = '/api';
+let baseURL = '/users';
 
 // You will need endpoints  delete a comment, delete a post, delete a user, update a user, post or comment.
 //get users
-app.get(`${baseURL}/user/`, userCtrl.getUser)
-
+// app.get(`${baseURL}`, userCtrl.getUser);
+app.post(`${baseURL}`, userCtrl.getUser);
 //get posts
-app.get(`${baseURL}/user/post`, userCtrl.getPost)
+app.get(`${baseURL}/post`, userCtrl.getPost);
 
 // add user
-app.post(`${baseURL}/user/`, userCtrl.addUser);
+app.post(`${baseURL}/add`, userCtrl.addUser);
 
 // add post
-app.post(`${baseURL}/user/post`, userCtrl.addPost);
+app.post(`${baseURL}/post`, userCtrl.addPost);
 
 // add comment
-app.post(`${baseURL}/user/post/comment`, userCtrl.addComment);
+app.post(`${baseURL}/post/comment`, userCtrl.addComment);
 
 // delete comment
-app.delete(`${baseURL}/user/post/comment/:commentid`, userCtrl.deleteComment);
+app.delete(`${baseURL}/post/comment/:commentid`, userCtrl.deleteComment);
 
 // Delete user entirely.
-app.delete(`${baseURL}/user/:userid`, userCtrl.deleteUser);
+app.delete(`${baseURL}/:userid`, userCtrl.deleteUser);
 
 // Update user data
-app.put(`${baseURL}/user/:id`, userCtrl.updateUser);
+app.put(`${baseURL}/:id`, userCtrl.updateUser);
 
 // update post
-app.put(`${baseURL}/user/post/:postid`, userCtrl.updatePost);
+app.put(`${baseURL}/post/:postid`, userCtrl.updatePost);
 
 // update comment
-app.put(`${baseURL}/user/post/comment/:commentid`, userCtrl.updateComment);
+app.put(`${baseURL}/post/comment/:commentid`, userCtrl.updateComment);
 
 app.listen(4000, () => {
 	console.log(`Server is now lying inside of 4000`);
