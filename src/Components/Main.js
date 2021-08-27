af448baedf8f074949d475e2a9c64632bd59c1f0
Megan <smith.megan.design@gmail.com>
2021-08-27 13:45:43 -0600

    refactoring entire app to react

---
+++ src/Components/Main.js
@@ -0,0 +1,9 @@
+function Main(){
+  return(
+		<div className="container grid">
+			<div className="left"></div>
+			<div className="middle"></div>
+			<div className="right"></div>
+		</div>
+)}
+export default Main
